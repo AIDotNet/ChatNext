139cbc4b8a91ea6f2144c6ed2319458070b827dd
Token <239573049@qq.com>
2024-10-28 17:10:36 +0800

    项目初始化

---
+++ src/ChatNext.EntityFrameworkCore/EntityFrameworkCore/ChatNextDbContext.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ChatNext.EntityFrameworkCore.EntityFrameworkCore;
+
+public class ChatNextDbContext(DbContextOptions<ChatNextDbContext> options) : DbContext(options)
+{
+    
+}
